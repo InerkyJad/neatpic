3acdec8d31e212745204b265b59c6732ec6d9196
Rayan Inerky <hassanaoutof@gmail.com>
2022-12-09 23:21:07 +0100

    added Blur

--- cmd/blur.go
+++ cmd/blur.go
@@ -1,8 +1,21 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+func blur(path string, degree float64) {
+	img := getImage(path)
+	img = imaging.Blur(img, degree)
+	err := imaging.Save(img, path)
+	if err != nil {
+		fmt.Println("Error while saving the image")
+		os.Exit(1)
+	}
+}
 
 // blurCmd represents the blur command
 var blurCmd = &cobra.Command{
@@ -12,9 +25,21 @@ var blurCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var path string = imagePathExists(args)
 		saveImage(path)
+		degree, err := cmd.Flags().GetFloat64("degree")
+		if err != nil {
+			fmt.Println("Error getting degree flag")
+			os.Exit(1)
+		}
+
+		if degree > 10 {
+			degree = 10
+		}
+
+		blur(path, degree)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(blurCmd)
+	blurCmd.Flags().Float64P("degree", "d", 1, "The degree of blur")
 }
