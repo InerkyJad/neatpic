4f7df0352b6a3bd36a0ed2e4d1ad9052d3286d03
Rayan Inerky <hassanaoutof@gmail.com>
2022-12-10 01:23:47 +0100

    adde resize

--- cmd/resize.go
+++ cmd/resize.go
@@ -1,8 +1,20 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+func resize(path string, width float64, height float64) {
+	img := getImage(path)
+	img = imaging.Resize(img, int(width), int(height), imaging.Lanczos)
+	err := imaging.Save(img, path)
+	if err != nil {
+		fmt.Println("Error while saving the image")
+	}
+}
 
 // resizeCmd represents the resize command
 var resizeCmd = &cobra.Command{
@@ -12,9 +24,27 @@ var resizeCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var path string = imagePathExists(args)
 		saveImage(path)
+		height, err := cmd.Flags().GetFloat64("height")
+		if err != nil {
+			fmt.Println("Error: unable to read height flag")
+			os.Exit(1)
+		}
+		width, err := cmd.Flags().GetFloat64("width")
+		if err != nil {
+			fmt.Println("Error: unable to read width flag")
+		}
+
+		if height == 0 && width == 0 {
+			fmt.Println("Error: you need to pass at least of of --height or --width")
+			os.Exit(1)
+		}
+
+		resize(path, width, height)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(resizeCmd)
+	resizeCmd.Flags().Float64P("height", "H", 0, "desired height")
+	resizeCmd.Flags().Float64P("width", "W", 0, "desired width")
 }
--- cmd/sharpen.go
+++ cmd/sharpen.go
@@ -1,8 +1,21 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+func sharpen(path string, value float64) {
+	img := getImage(path)
+	img = imaging.Sharpen(img, value)
+	err := imaging.Save(img, path)
+	if err != nil {
+		fmt.Println("Error while saving the image")
+		os.Exit(1)
+	}
+}
 
 // sharpenCmd represents the sharpen command
 var sharpenCmd = &cobra.Command{
@@ -12,9 +25,21 @@ var sharpenCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var path string = imagePathExists(args)
 		saveImage(path)
+		value, err := cmd.Flags().GetFloat64("value")
+		if err != nil {
+			fmt.Println("Error: cannot get value")
+		}
+
+		if value < 0 {
+			fmt.Println("Error: value must be greater than 0")
+			os.Exit(1)
+		}
+
+		sharpen(path, value)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sharpenCmd)
+	sharpenCmd.Flags().Float64P("value", "v", 1, "The sharpen value (default is 1)")
 }
