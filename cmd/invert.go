06ad04e0bbfd2c2b84fc9b9f1ac7e5d93152a292
Rayan Inerky <hassanaoutof@gmail.com>
2022-12-10 01:44:20 +0100

    Fixed Invert

--- cmd/invert.go
+++ cmd/invert.go
@@ -1,8 +1,21 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+func invert(path string) {
+	img := getImage(path)
+	img = imaging.Invert(img)
+	err := imaging.Save(img, path)
+	if err != nil {
+		fmt.Println("Error while saving the image")
+		os.Exit(1)
+	}
+}
 
 // invertCmd represents the invert command
 var invertCmd = &cobra.Command{
@@ -12,6 +25,7 @@ var invertCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var path string = imagePathExists(args)
 		saveImage(path)
+		invert(path)
 	},
 }
 
